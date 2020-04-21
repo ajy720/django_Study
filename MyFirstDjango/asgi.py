34a7df212d52f3e173684411cfe6360a46d8ad9c
AnHyeonSeok <ajy720@gmail.com>
2020-04-21 22:51:47 +0900

    Admin page

--- MyFirstDjango/asgi.py
+++ MyFirstDjango/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for my_site_prj project.
+ASGI config for MyFirstDjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_site_prj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFirstDjango.settings')
 
 application = get_asgi_application()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # django_Study
+
+## run server
+> sh ./runserver
+
+## DB migrate all in one
+> sh ./migrate
