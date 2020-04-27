5532bdb45224bb56bf030d0dcae469168cb7fa6c
AnHyeonSeok <ajy720@gmail.com>
2020-04-27 17:07:23 +0900

    read data, template for, order data

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+
 # Create your models here.
 
 
 class Post(models.Model):
+    class Meta:
+        ordering = ["-created_at"]  # 정렬 방법 1
+        pass
+
     title = models.CharField(max_length=50)
     content = models.TextField()
     view_count = models.IntegerField(default=0)
--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,11 @@ import pdb
 
 
 def main(request):
-    return render(request, "posts/main.html")
+    context = {
+        "posts": Post.objects.all(),  # 정렬 방법 1. models.py에서 해주는 방법
+        # "posts": Post.objects.order_by("-created_at"), # 정렬 방법 2
+    }
+    return render(request, "posts/main.html", context)
 
 
 def new(request):
